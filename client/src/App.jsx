644db58765a670c72766a368bcc5fbfe4a0ba371
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2023-11-14 19:51:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,19 @@ import { Home, CreatePost } from './pages';
 const App = () => (
   <BrowserRouter>
 
-    {/* header: contains create button linked to create-post page(/create-post), and openai logo which 
-    links to homepage (/).  */}
+    
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
-      {/* linked button created below:  */}
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      <div>
+        <h1 className="openai-font">Clone created by Akash Kamat</h1>
+      </div>
     </header>
 
-    {/* Main content of the page. */}
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       
-      {/* Routes is component from React Router DOM that serves
-       as a container for defining multiple Route components. 
-       here, route to homepage and route to createpost has been created.*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
